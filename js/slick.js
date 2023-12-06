727f6803beeb7ed20a8f56c617ccd2f35b2e48c2
softwarenur@gmail.com <nur42658@gmail.com>
2023-12-06 19:21:37 +0600

    product details some updated

--- js/slick.js
+++ js/slick.js
@@ -4,7 +4,10 @@ $(".slider-for").slick({
   arrows: false,
   fade: false,
   asNavFor: ".slider-nav",
+  prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+  nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
 });
+
 $(".slider-nav").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
